5584dd00dedc5e6a56dd5137e858b0b227cc1d5c
Tom Whittle <thomas.whittle@andersen-ev.com>
2021-11-08 11:28:43 +0000

    initial auth setup using cognito and amplify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.min.css';
 import './index.css';
 import App from './App';
+import Amplify from 'aws-amplify';
+import config from './config'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
